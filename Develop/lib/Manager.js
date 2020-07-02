20cb5eaeb30b2d16fa7577bbf047b8fee6372d95
min <pakmk8@gmail.com>
2020-07-02 17:57:08 -0400

    tests pass for manager, engineer, and intern

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// Subclass
+// 20-Ins-Subclasses
+const Employee = require(`./employee`);
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+  getRole() {
+    return `Manager`;
+  }
+}
+
+module.exports = Manager;
